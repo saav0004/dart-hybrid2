220c9c2cf40607683b79b6a59967f8253d2a5c81
Luciano Saavedra <saav0004@algonquinlive.com>
2023-09-28 18:17:17 -0400

    erase comments

--- lib/hybrid2.dart
+++ lib/hybrid2.dart
@@ -8,10 +8,6 @@ class Lucky {
     String minEnv = Platform.environment['MIN'] ?? '10';
     String maxEnv = Platform.environment['MAX'] ?? '100';
 
-    // print(minEnv);
-    // print(maxEnv);
-
-    //parsed
     int min = int.parse(minEnv);
     int max = int.parse(maxEnv);
     randomNum = Random().nextInt(max - min) + min;
